78cf170a2af321faf633d24fe23a9063096e2487
antmhernandez <ant.m.hernandez@gmail.com>
2020-05-05 10:01:10 -0400

    Update source.cpp
    Added main

---
+++ source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+//I want to make a file class. -TH
+//it will include easyFileName, fileName as members of objects in an object array
+
+int main()
+{
+
+
+    return 0;
+}
