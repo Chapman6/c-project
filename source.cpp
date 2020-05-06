4018808887748751b55d64e797542070eea49dd1
antmhernandez <ant.m.hernandez@gmail.com>
2020-05-06 09:02:09 -0400

    Revert delete.

--- source.cpp
+++ source.cpp
@@ -7,14 +7,14 @@ using namespace std;
 //I want to make a file class. -TH
 //it will include easyFileName, fileName as members of objects in an object array
 
-//string test;
+
+int main()
+{
+string test;
 ifstream studentData;
 studentData.open("studentData.txt", ios::app);
 studentData >> test;
 cout << test;
-int main()
-{
-
 
     return 0;
 }
