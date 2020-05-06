25691cd1bdf98d54927355a33f9adf0978aa8b0f
antmhernandez <ant.m.hernandez@gmail.com>
2020-05-06 08:21:58 -0400

    Trouble with string and ifstream packages

--- source.cpp
+++ source.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 
 using namespace std;
 
 //I want to make a file class. -TH
 //it will include easyFileName, fileName as members of objects in an object array
 
+//string test;
+ifstream studentData;
+studentData.open("studentData.txt", ios::app);
+studentData >> test;
+cout << test;
 int main()
 {
 
